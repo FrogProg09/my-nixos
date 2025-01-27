825d45804b5a0467c9061ca2594f69b5b5d3776c
frogprog <frogprog09@gmail.com>
2025-01-27 13:20:32 +0400

    emojis

--- awesome_setup.sh
+++ awesome_setup.sh
@@ -3,35 +3,35 @@
 # deleting yours deafult configs that u have and copying from this repo
 if test -f /etc/nixos/configuration.nix; then  # checking if configuration.nix file exists
 
-  echo "found configuration.nix, deleting..."
+  echo "found configuration.nix, deleting... (づ ◕‿◕ )づ"
   sudo rm /etc/nixos/configuration.nix
 
   if [ $? -eq 0 ]; then   # checking if last executed command was successful
-    echo "old configuration.nix was successfully removed"
+    echo "old configuration.nix was successfully removed  ٩(◕‿◕)۶"
   else
-    echo "could not delete old configuration file"
+    echo "could not delete old configuration file ᕙ(⇀‸↼‶)ᕗ"
     exit 1
   fi
 
-  echo "copying configuration.nix from this repo..."
+  echo "copying configuration.nix from this repo... (づ ◕‿◕ )づ"
   sudo cp ~/my-nixos/configuration.nix /etc/nixos/
 
   if [ $? -eq 0 ]; then   # checking if last executed command was successful
-    echo "Installed configuration.nix successfully"
+    echo "Installed configuration.nix successfully  ٩(◕‿◕)۶"
   else
-    echo "Could not copy configuration.nix from this repo"
+    echo "Could not copy configuration.nix from this repo ᕙ(⇀‸↼‶)ᕗ"
     exit 1
   fi
   
 else
 
-  echo "Did not find configuration.nix, copying from this repo..."
+  echo "Did not find configuration.nix, copying from this repo... (づ ◕‿◕ )づ"
   sudo cp ~/my-nixos/configuration.nix /etc/nixos/
   
   if [ $? -eq 0 ]; then   # checking if last executed command was successful
-    echo "Installed configuration.nix successfully"
+    echo "Installed configuration.nix successfully  ٩(◕‿◕)۶"
   else
-    echo "Could not copy configuration.nix from this repo"
+    echo "Could not copy configuration.nix from this repo ᕙ(⇀‸↼‶)ᕗ"
     exit 1
   fi
 fi
@@ -40,38 +40,38 @@ fi
 
 if test -d ~/.config/hypr/; then
   
-  echo "~/.config/hypr/ directory exists, deleting..."
+  echo "~/.config/hypr/ directory exists, deleting... (づ ◕‿◕ )づ"
   rm -r ~/.config/hypr/
   
 fi
 if test -d ~/.config/waybar/; then
 
-  echo "~/.config/waybar/ directory exists, deleting..."
+  echo "~/.config/waybar/ directory exists, deleting... (づ ◕‿◕ )づ"
   rm -r ~/.config/waybar/
   
 fi
 if test -d ~/.config/kitty/; then
 
-  echo "~/.config/kitty/ directory exists, deleting..."
+  echo "~/.config/kitty/ directory exists, deleting... (づ ◕‿◕ )づ"
   rm -r ~/.config/kitty/
 
 fi
 if test -d ~/.config/lvim/; then
 
-  echo "~/.config/lvim/ directory exists, deleting..."
+  echo "~/.config/lvim/ directory exists, deleting... (づ ◕‿◕ )づ"
   rm -r ~/.config/lvim/
 
 fi
 if test -f ~/.config/starship.toml; then
 
-  echo "~/.config/starship.toml file exists, deleting..."
+  echo "~/.config/starship.toml file exists, deleting... (づ ◕‿◕ )づ"
   rm ~/.config/starship.toml
 
 fi
 
 # installing configs of programs from this repo
 
-echo "installing configs into ~/.config/ directory..."
+echo "installing configs into ~/.config/ directory... (づ ◕‿◕ )づ"
 cp -r ~/my-nixos/DOTFILES/.config/hypr/ ~/.config/
 cp -r ~/my-nixos/DOTFILES/.config/waybar/ ~/.config/
 cp -r ~/my-nixos/DOTFILES/.config/kitty/ ~/.config/
@@ -81,17 +81,18 @@ cp ~/my-nixos/DOTFILES/.config/starship.toml ~/.config/
 # also putting wallpapers directory to ~/Pictures/
 
 if test -d ~/Pictures/; then
-  echo "Moving wallpapers to Pictures directory"
+  echo "Moving wallpapers to Pictures directory... (づ ◕‿◕ )づ"
   mv wallpapers ~/Pictures/
 else
-  echo "Did not find Pictures directory.. creating..."
+  echo "Did not find Pictures directory.. creating... (づ ◕‿◕ )づ"
   mkdir ~/Pictures/
+  echo "Moving wallpapers to Pictures directory... (づ ◕‿◕ )づ"
   mv wallpapers ~/Pictures/
 fi
 
 # for working rofi
 
-echo "Installing extra rofi repo..."
+echo "Installing extra rofi repo... (づ ◕‿◕ )づ"
 
 cd
 git clone --depth=1 https://github.com/adi1090x/rofi.git
--- only_nix_setup.sh
+++ only_nix_setup.sh
@@ -4,37 +4,37 @@
 
 if test -f /etc/nixos/configuration.nix; then # checking if configuration.nix exists
 
-  echo "Found configuration.nix, deleting..."
+  echo "Found configuration.nix, deleting... (づ ◕‿◕ )づ"
   sudo rm /etc/nixos/configuration.nix
 
   if [ $? -eq 0 ]; then  # checking if last command was successful
 
-    echo "Old configuration was deleted successfully"
-    echo "Installing new config to /etc/nixos/"
+    echo "Old configuration was deleted successfully  ٩(◕‿◕)۶"
+    echo "Installing new config to /etc/nixos/  (づ ◕‿◕ )づ"
 
     sudo cp ~/my-nixos/configuration.nix /etc/nixos/ # copying configuration.nix file to its place
 
     if [ $? -eq 0 ]; then  # checking if last command was successful
-      echo "New configuration.nix file Installed successfully!"
+      echo "New configuration.nix file Installed successfully!  ٩(◕‿◕)۶"
     else
-      echo "Could not copy configuration.nix from this repo"
+      echo "Could not copy configuration.nix from this repo ᕙ(⇀‸↼‶)ᕗ"
       exit 1
     fi
 
   else
-    echo "Could not delete configuration.nix file"
+    echo "Could not delete configuration.nix file ᕙ(⇀‸↼‶)ᕗ"
     exit 1
   fi
 
 else 
   
-  echo "Did not found configuration.nix, copying from repo..."
+  echo "Did not found configuration.nix, copying from repo... (づ ◕‿◕ )づ"
   sudo cp ~/my-nixos/configuration.nix /etc/nixos/
 
   if [ $? -eq 0 ]; then  # checking if last command was successful
-    echo "Installed successfully!"
+    echo "Installed successfully!   ٩(◕‿◕)۶"
   else 
-    echo "Could not copy configuration.nix"
+    echo "Could not copy configuration.nix ᕙ(⇀‸↼‶)ᕗ"
     exit 1
   fi
 fi
